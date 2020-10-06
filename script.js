9c49c061dc2a4634a8a613ed11e51693b862384a
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-06 17:21:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const downloadButton = document.getElementById('download-project-button');
 const projectURL = document.getElementById('project-url');
 
 downloadButton.onclick = function() {
-  if(project-url.value.trim().length >= 1) {
+  if(projectURL.value.trim().length >= 1) {
     let params = {};
     const urlParams = projectURL.value.replace('https://snap.berkeley.edu/snap/snap.html#present:','').split('&');
     urlParams.forEach((item, index) => {
       urlParams[index] = item.split('=');
       params[item] = item[1]; 
     });
-    document.getElementById('project-iframe').src = project-url.value;
+    const projectIFrame = document.getElementById('project-iframe');
+    projectIFrame.style.display = 'block';
+    projectIFrame.src = projectURL.value;
     fetch('https://cors-anywhere.herokuapp.com/').then(res => {
       document.getElementById('project-notes').value = res.json().notes;
     });
