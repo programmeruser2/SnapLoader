1db6fae85996d3595687996673125cacea71c9a8
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-07 09:30:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
-const downloadButton = document.getElementById('download-project-button');
-const projectURL = document.getElementById('project-url');
+const $ = function(id) { return document.getElementById(id); };
+const downloadButton = $('download-project-button');
+const projectURL = $('project-url');
+const projectName = $('project-name');
+const projectAuthor = $('project-author');
 
 downloadButton.onclick = function() {
   if(projectURL.value.trim().length >= 1) {
@@ -10,15 +13,15 @@ downloadButton.onclick = function() {
       urlParams[index] = item.split('=');
       params[urlParams[index][0]] = urlParams[index][1]; 
     });
-    const projectIFrame = document.getElementById('project-iframe');
+    const projectIFrame = $('project-iframe');
     projectIFrame.style.display = 'block';
     projectIFrame.src = projectURL.value;
     (async function() {
       const req = await fetch(`https://cors-anywhere.herokuapp.com/https://cloud.snap.berkeley.edu/projects/${params.Username}/${params.ProjectName}/metadata`)
       const data = await req.json();
-      const projectNotes = await document.getElementById('project-notes');
+      const projectNotes = await $('project-notes');
       projectNotes.style.display = 'inline-block';
       projectNotes.value = data.notes || '';  
    })();
-  }
+  } else if()
 }
