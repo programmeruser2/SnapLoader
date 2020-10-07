49c9a8b71041cfb34277ab8fb1f65b624e664376
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-07 14:18:12 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const projectURL = $('project-url');
 const projectName = $('project-name');
 const projectAuthor = $('project-author');
 const projectIFrame = $('project-iframe');
+const projectNotes = $('project-notes');
 
 function isEmpty(str) {
   if(str.trim().length >= 1) {
@@ -17,7 +18,12 @@ downloadButton.onclick = function() {
   if(!isEmpty(projectName.value) && !isEmpty(projectAuthor.value)) {
     projectIFrame.style.display = 'block';
     projectIFrame.src = `https://snap.berkeley.edu/snap/snap.html#present:Username=${encodeURIComponent(projectAuthor.value)}&ProjectName=${encodeURIComponent(projectName.value)}`;
-    
+    (async function() {
+      const req = await fetch(`https://cors-anywhere.herokuapp.com/https://cloud.snap.berkeley.edu/projects/${params.Username}/${params.ProjectName}/metadata`)
+      const data = await req.json();
+      projectNotes.style.display = 'inline-block';
+      projectNotes.value = data.notes || '';  
+    })();
   } else if(!isEmpty(projectURL.value)) {
     let params = {};
     const urlParams = projectURL.value.replace('https://snap.berkeley.edu/snap/snap.html#present:','').split('&');
@@ -30,9 +36,8 @@ downloadButton.onclick = function() {
     (async function() {
       const req = await fetch(`https://cors-anywhere.herokuapp.com/https://cloud.snap.berkeley.edu/projects/${params.Username}/${params.ProjectName}/metadata`)
       const data = await req.json();
-      const projectNotes = await $('project-notes');
       projectNotes.style.display = 'inline-block';
       projectNotes.value = data.notes || '';  
-   })();
+    })();
   }
 }
