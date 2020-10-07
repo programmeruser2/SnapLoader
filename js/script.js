1b260bbe8e8e1053ec7b81b96de1dd3197696f13
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-07 14:30:52 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,16 +14,17 @@ function isEmpty(str) {
     return true;
   }
 }
+async function updateProjectNotes(project, username) {
+  const req = await fetch(`https://cors-anywhere.herokuapp.com/https://cloud.snap.berkeley.edu/projects/${encodeURIComponent(projectAuthor.value)}/${encodeURIComponent(projectName.value)}/metadata`)
+  const data = await req.json();
+  projectNotes.style.display = 'inline-block';
+  projectNotes.value = data.notes || '';  
+}
 downloadButton.onclick = function() {
   if(!isEmpty(projectName.value) && !isEmpty(projectAuthor.value)) {
     projectIFrame.style.display = 'block';
     projectIFrame.src = `https://snap.berkeley.edu/snap/snap.html#present:Username=${encodeURIComponent(projectAuthor.value)}&ProjectName=${encodeURIComponent(projectName.value)}`;
-    (async function() {
-      const req = await fetch(`https://cors-anywhere.herokuapp.com/https://cloud.snap.berkeley.edu/projects/${encodeURIComponent(projectAuthor.value)}/${encodeURIComponent(projectName.value)}/metadata`)
-      const data = await req.json();
-      projectNotes.style.display = 'inline-block';
-      projectNotes.value = data.notes || '';  
-    })();
+    updateProjectNotes();
   } else if(!isEmpty(projectURL.value)) {
     let params = {};
     const urlParams = projectURL.value.replace('https://snap.berkeley.edu/snap/snap.html#present:','').split('&');
@@ -33,11 +34,6 @@ downloadButton.onclick = function() {
     });
     projectIFrame.style.display = 'block';
     projectIFrame.src = projectURL.value;
-    (async function() {
-      const req = await fetch(`https://cors-anywhere.herokuapp.com/https://cloud.snap.berkeley.edu/projects/${params.Username}/${params.ProjectName}/metadata`)
-      const data = await req.json();
-      projectNotes.style.display = 'inline-block';
-      projectNotes.value = data.notes || '';  
-    })();
+    updateProjectNotes();
   }
 }
